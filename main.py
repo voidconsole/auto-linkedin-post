1e4c29b4d11ac23d3285f7c63b552f891550f942
Satwik Bhusanur <152626693+voidconsole@users.noreply.github.com>
2025-03-21 01:39:59 +0530

    debugging errors

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ API_KEY = os.environ.get('GOOGLE_API_KEY')
 CLIENT_ID = os.environ.get('LINKEDIN_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('LINKEDIN_CLIENT_SECRET')
 ACCESS_TOKEN = os.environ.get('LINKEDIN_ACCESS_TOKEN')
-
 url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
 
-print(f"prompt:{PROMPT}")
 def get_ai_data(prompt):
 	payload = {
 	"contents": [
